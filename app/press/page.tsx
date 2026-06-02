3dfc179fa9f9b410d0920d5754832822bb89f7a4
Alex Carlson <alex-carlson@users.noreply.github.com>
2026-06-02 12:04:59 -0400

    updated gif formatting

--- app/press/page.tsx
+++ app/press/page.tsx
@@ -226,7 +226,7 @@ export default function Press() {
 
                             <div>
                                 <h3 className="text-lg font-medium mb-3">GIFs</h3>
-                                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                                     <div >
                                         <Image
                                             src="/gif4.gif"
